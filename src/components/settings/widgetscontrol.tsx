936616992ae51d6b3c14296bf10e793cf80b847e
Rashidnashtar <Rashidmohamadnashtar2002@gmail.com>
2023-01-18 18:10:16 +0300

    fixing issue

--- src/components/settings/widgetscontrol.tsx
+++ src/components/settings/widgetscontrol.tsx
@@ -4,27 +4,27 @@ const WidgetsControl = () => {
       <h3>Widgets Control</h3>
       <p>Show/Hide Widgets</p>
       <div className="chose">
-        <input type="checkbox" name="quick" />
+        <input type="checkbox" id="quick" />
         <label htmlFor="quick">Quick Draft</label>
       </div>
       <div className="chose">
-        <input type="checkbox" name="yearly" />
+        <input type="checkbox" id="yearly" />
         <label htmlFor="yearly">Yearly Targets</label>
       </div>
       <div className="chose">
-        <input type="checkbox" name="tickets" />
+        <input type="checkbox" id="tickets" />
         <label htmlFor="tickets">Tickets Statistics</label>
       </div>
       <div className="chose">
-        <input type="checkbox" name="latest" />
+        <input type="checkbox" id="latest" />
         <label htmlFor="latest">Latest News</label>
       </div>
       <div className="chose">
-        <input type="checkbox" name="tasks" />
+        <input type="checkbox" id="tasks" />
         <label htmlFor="tasks">Latest Tasks</label>
       </div>
       <div className="chose">
-        <input type="checkbox" name="top" />
+        <input type="checkbox" id="top" />
         <label htmlFor="top"> Top Search Items</label>
       </div>
     </div>
